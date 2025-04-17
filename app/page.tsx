1a32e53ff8dbe840d904ef8a415561c6ec29b4d5
Sam Hurst <samohurst@gmail.com>
2025-04-17 22:14:56 +0100

    auth added

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,15 @@ import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
+import { useAuthenticator } from "@aws-amplify/ui-react";
 
 Amplify.configure(outputs);
 
 const client = generateClient<Schema>();
-
+  
 export default function App() {
+  const { user, signOut } = useAuthenticator();
+
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   function listTodos() {
@@ -33,7 +36,7 @@ export default function App() {
 
   return (
     <main>
-      <h1>My todos</h1>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
@@ -47,6 +50,8 @@ export default function App() {
           Review next steps of this tutorial.
         </a>
       </div>
+
+      <button onClick={signOut}>Sign out</button>
     </main>
   );
 }

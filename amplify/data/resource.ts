2dc2a6405c715531c077a37dfa08cad6e2695667
Sam Hurst <samohurst@gmail.com>
2025-04-17 23:37:31 +0100

    tiny

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -11,8 +11,7 @@ const schema = a.schema({
     .model({
       content: a.string(),
     })
-    // .authorization((allow) => [allow.publicApiKey()]),
-  .authorization(allow => [allow.owner()]),
+    .authorization(allow => [allow.owner()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -20,7 +19,7 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: "userPool",
+    defaultAuthorizationMode: 'userPool',
     apiKeyAuthorizationMode: {
       expiresInDays: 30,
     },
